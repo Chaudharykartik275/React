c1c3b062afaa70e68270605045b0b556ca5c2c70
ChaudharyKartik275 <kc753742@gmail.com>
2025-05-02 12:49:27 +0530

    Introduction React

---
+++ Introduction.js
@@ -0,0 +1,21 @@
+// React is JS library for created by facebook.
+// Most popular library for building user interfaces.
+// Also used by Instagram, Netflix, Airbnb, WhatsApp, and more.
+// Used to create single page applications (SPAs).
+// We can build modern, responsive, and fast web applications with React.
+
+
+"//What is a Library?"
+// A library is a collection of pre-written code that can be used to perform common tasks.
+// We use libraries to save time and effort when writing code.
+// We have the flexibility to use libraries in any way we want.
+// example: Lodash, jQuery, etc.
+
+
+"// What is a Framework?"
+// A framework is a collection of libraries and tools that provide a structure for building applications.
+// A framework provides a set of rules and guidelines for building applications.
+// A framework is more opinionated than a library and provides a specific way of doing things.
+// A framework has its own set of rules and guidelines for building applications.
+// We don't have much flexibility in how we build our applications with a framework.
+// example: Angular, Vue.js, etc.
